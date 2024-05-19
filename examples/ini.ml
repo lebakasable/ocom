bcb045eabc483caeacfa75c0aa947c57e03ae85b
LeBakaSable <hugogole.ito@gmail.com>
2024-05-20 00:07:33 +0200

    feat: parse ini from file

--- examples/ini.ml
+++ examples/ini.ml
@@ -8,6 +8,27 @@ type section_t =
   ; pairs : pair_t list
   }
 
+let show_pair ((key, value) : pair_t) : string =
+  Printf.sprintf "(%s, %s)" key value
+
+let show_pairs (pairs : pair_t list) : string =
+  pairs
+  |> List.map show_pair
+  |> String.concat ","
+  |> Printf.sprintf "[%s]"
+
+let show_section (section : section_t) : string =
+  Printf.sprintf
+  	"{ name = %s; pairs = %s }"
+  	section.name
+  	(show_pairs section.pairs)
+
+let show_sections (sections : section_t list) : string =
+  sections
+  |> List.map show_section
+  |> String.concat ","
+  |> Printf.sprintf "[%s]"
+
 let read_whole_file (file_path : string) : string =
 	let ch = open_in file_path in
 	let n = in_channel_length ch in
@@ -16,7 +37,7 @@ let read_whole_file (file_path : string) : string =
 	s
 
 let section_name : string parser =
-  prefix "[" *> parse_while ((!=) ']') <* prefix "]"
+  prefix "[" *> parse_while (( != ) ']') <* prefix "]"
 
 let is_space (x : char) = x == ' ' || x == '\n'
 
@@ -33,3 +54,19 @@ let section: section_t parser =
 
 let ini: section_t list parser =
   many section
+
+let () =
+  match Sys.argv |> Array.to_list with
+  | _ :: file_path :: _ ->
+		let result = file_path
+              	 |> read_whole_file
+              	 |> make_input
+              	 |> ini.run
+    in
+    (match result with
+    | Ok (_, sections) -> sections |> show_sections |> print_endline
+    | Error error -> Printf.printf
+    								   "Error during parding at position %d: %s\n"
+                       error.pos
+                       error.desc)
+  | _ -> failwith "Expected path to an ini file"
